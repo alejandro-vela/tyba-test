dfc2802d4265b0365b4497a7f8e5767890a411b9
alejoAnt <carlos.vela@antpack.co>
2022-09-10 11:53:39 -0500

    Function to listen lists

--- lib/src/pages/home_screen.dart
+++ lib/src/pages/home_screen.dart
@@ -28,6 +28,7 @@ class _HomeScreenState extends State<HomeScreen> {
   late UniversityBloc _bloc = UniversityBloc();
   bool isLoading = false;
   bool _gridFlag = true;
+  final ScrollController _controller = ScrollController();
 
   @override
   void initState() {
@@ -92,93 +93,113 @@ class _HomeScreenState extends State<HomeScreen> {
       );
 
   Widget _list(BuildContext context, List<UniversityModel> data) {
-    return ListView.builder(
-      itemCount: data.length,
-      physics: BouncingScrollPhysics(),
-      itemBuilder: (BuildContext context, int index) => Container(
-        height: 7.h,
-        width: 90.w,
-        margin: EdgeInsets.symmetric(vertical: 2.h, horizontal: 2.w),
-        decoration: BoxDecoration(
-          color: AppColors.primaryLigthColor,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: InkWell(
-          onTap: (() {
-            navigateToDetailScreen(context, _bloc, index);
-          }),
-          child: ListTile(
-            leading: data[index].image != null
-                ? SizedBox(
-                    height: 60,
-                    width: 50,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(180),
-                      child: Image.file(
-                        File(data[index].image!),
-                      ),
-                    ),
-                  )
-                : SizedBox(
-                    height: 60,
-                    width: 50,
-                    child: Image(
-                      fit: BoxFit.scaleDown,
-                      height: 4.h,
-                      image: NetworkImage(
-                          'https://cdn-icons-png.flaticon.com/512/154/154378.png?w=1380&t=st=1662821833~exp=1662822433~hmac=b58c788c8bc7096d4bbe8e61fec61d731164dd245ab07033ae8a73c5ff77b412'),
-                    ),
-                  ),
-            title: Text(data[index].name),
-            trailing: Icon(Icons.arrow_forward_ios),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _grid(BuildContext context, List<UniversityModel> data) {
-    return GridView.count(
-      crossAxisCount: 2,
-      childAspectRatio: 3.6 / 4,
-      children: List.generate(data.length, (index) {
-        return Center(
-          child: Card(
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-            color: AppColors.primaryLigthColor,
-            margin: EdgeInsets.all(15),
-            elevation: 10,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(30),
-              child: SizedBox(
-                width: 100.w,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    data[index].image != null
-                        ? Image.file(File(data[index].image!))
-                        : Image(
-                            fit: BoxFit.scaleDown,
-                            height: 13.h,
-                            image: NetworkImage(
-                                'https://cdn-icons-png.flaticon.com/512/154/154378.png?w=1380&t=st=1662821833~exp=1662822433~hmac=b58c788c8bc7096d4bbe8e61fec61d731164dd245ab07033ae8a73c5ff77b412'),
+    return NotificationListener<ScrollEndNotification>(
+        onNotification: (notification) {
+          if (_controller.position.maxScrollExtent ==
+              _controller.position.pixels) {
+            //TODO me falto implementar la logica para el scroll infinito
+          }
+          return true;
+        },
+        child: ListView.builder(
+          controller: _controller,
+          itemCount: data.length,
+          physics: BouncingScrollPhysics(),
+          itemBuilder: (BuildContext context, int index) => Container(
+            height: 7.h,
+            width: 90.w,
+            margin: EdgeInsets.symmetric(vertical: 2.h, horizontal: 2.w),
+            decoration: BoxDecoration(
+              color: AppColors.primaryLigthColor,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: InkWell(
+              onTap: (() {
+                navigateToDetailScreen(context, _bloc, index);
+              }),
+              child: ListTile(
+                leading: data[index].image != null
+                    ? SizedBox(
+                        height: 60,
+                        width: 50,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(180),
+                          child: Image.file(
+                            File(data[index].image!),
                           ),
-                    Container(
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        data[index].name,
-                        textAlign: TextAlign.center,
+                        ),
+                      )
+                    : SizedBox(
+                        height: 60,
+                        width: 50,
+                        child: Image(
+                          fit: BoxFit.scaleDown,
+                          height: 4.h,
+                          image: NetworkImage(
+                              'https://cdn-icons-png.flaticon.com/512/154/154378.png?w=1380&t=st=1662821833~exp=1662822433~hmac=b58c788c8bc7096d4bbe8e61fec61d731164dd245ab07033ae8a73c5ff77b412'),
+                        ),
                       ),
-                    ),
-                  ],
-                ),
+                title: Text(data[index].name),
+                trailing: Icon(Icons.arrow_forward_ios),
               ),
             ),
           ),
-        );
-      }),
+        ));
+  }
+
+  Widget _grid(BuildContext context, List<UniversityModel> data) {
+    return NotificationListener<ScrollEndNotification>(
+      onNotification: (notification) {
+        if (_controller.position.maxScrollExtent ==
+            _controller.position.pixels) {
+          //TODO me falto implementar la logica para el scroll infinito
+        }
+
+        return true;
+      },
+      child: GridView.count(
+        crossAxisCount: 2,
+        controller: _controller,
+        childAspectRatio: 3.6 / 4,
+        children: List.generate(data.length, (index) {
+          return Center(
+            child: Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30)),
+              color: AppColors.primaryLigthColor,
+              margin: EdgeInsets.all(15),
+              elevation: 10,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(30),
+                child: SizedBox(
+                  width: 100.w,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      data[index].image != null
+                          ? Image.file(File(data[index].image!))
+                          : Image(
+                              fit: BoxFit.scaleDown,
+                              height: 13.h,
+                              image: NetworkImage(
+                                  'https://cdn-icons-png.flaticon.com/512/154/154378.png?w=1380&t=st=1662821833~exp=1662822433~hmac=b58c788c8bc7096d4bbe8e61fec61d731164dd245ab07033ae8a73c5ff77b412'),
+                            ),
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          data[index].name,
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        }),
+      ),
     );
   }
 }
