836d0421cdf3fd844570224b5175ec8aa3417fb3
alejoAnt <carlos.vela@antpack.co>
2022-09-10 09:40:11 -0500

    update routes

--- lib/src/utils/routes/navigation.dart
+++ lib/src/utils/routes/navigation.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:tyba_test/src/pages/detail_screem.dart';
+import '../../bloc/university_bloc/university_bloc.dart';
 import '../../pages/home_screen.dart';
 
 // Home
-navigateToHome(BuildContext context, bool remplaced) => remplaced
-    ? Navigator.of(context).pushAndRemoveUntil(
+navigateToHome(
+        BuildContext context, bool remplaced, UniversityBloc? universityBloc) =>
+    remplaced
+        ? Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (BuildContext contextt) {
+            return HomeScreen(newBLoc: universityBloc);
+          }), (Route route) => false)
+        : Navigator.of(context).pushNamed(HomeScreen.routeName);
+
+navigateToDetailScreen(
+        BuildContext context, UniversityBloc universityBloc, int index) =>
+    Navigator.of(context).pushAndRemoveUntil(
         MaterialPageRoute(builder: (BuildContext contextt) {
-        return HomeScreen();
-      }), (Route route) => false)
-    : Navigator.of(context).pushNamed(HomeScreen.routeName);
+      return DetailScreen(
+        universityBloc: universityBloc,
+        index: index,
+      );
+    }), (Route route) => false);
--- lib/src/utils/routes/routes.dart
+++ lib/src/utils/routes/routes.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:tyba_test/src/pages/detail_screem.dart';
 import '../../pages/home_screen.dart';
 
 Map<String, Widget Function(BuildContext)> routes = {
   HomeScreen.routeName: (context) => HomeScreen(),
+  DetailScreen.routeName: (context) => DetailScreen(),
 };
